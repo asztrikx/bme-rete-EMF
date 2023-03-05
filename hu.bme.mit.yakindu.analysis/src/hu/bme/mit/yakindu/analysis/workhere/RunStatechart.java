b54db1ed57e32233d6f51b00d447d4ae18644685
Your Name <you@example.com>
2023-03-05 12:04:50 +0100

    3.3 & 3.4 & 3.5

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,31 +1,51 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
-
-
 
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		
+		s.init();
+		s.enter();
+		s.runCycle();
+		
+		print(s);
+		
+		while(true) {
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		    boolean exit = false;
+			String input = br.readLine();
+			switch(input) {
+				case "start":
+					s.raiseStart();
+					break;
+				case "white":
+					s.raiseWhite();
+					break;
+				case "black":
+					s.raiseBlack();
+					break;
+				case "exit":
+					exit = true;
+					s.exit();
+					break;
+			}
+			s.runCycle();
+			print(s);
+			if (exit) break;
+		}
+		System.exit(0);
 	}
 
 	public static void print(IExampleStatemachine s) {
